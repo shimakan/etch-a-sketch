b09422afa3c5756d57cc94ed680bae14e52cd527
david <72994273+shimakan@users.noreply.github.com>
2025-01-06 12:45:24 -0800

    implement dynamic grid size

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,35 @@
-function createGrid(rows, cols){
-    const container = document.querySelector('.container');
 
-    for(let i = 0; i < rows; i++){
-        for(let j = 0; j < cols; j++){
+const container = document.querySelector('.container');
+const resizeButton = document.querySelector("#resize-button");
+
+function createGrid(size){
+    //clear existing html
+    container.innerHTML = "";
+    
+    const gridItemSize = 960 / size;
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.style.width = `${gridItemSize}px`;
+            cell.style.height = `${gridItemSize}px`;
+            cell.addEventListener("mouseenter", () => {
+                cell.classList.add("hovered"); // add a class to change the color
+            });
+
             container.appendChild(cell);
         }
     }
 }
 
-createGrid(4, 4);
+resizeButton.addEventListener("click", () => {
+    let newSize = prompt("Enter the number of squares per side (1-100):  ");
+    newSize = parseInt(newSize);
+    if(isNaN(newSize) || newSize < 1 || newSize > 100){
+        alert("Enter a valid number between 1-100.");
+    }else{
+        createGrid(newSize);
+    }
+});
+
+createGrid(4); // default grid size
